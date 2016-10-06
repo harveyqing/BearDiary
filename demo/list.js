aacb58a13a356e591359e79b7c0c46a288cdfd23
wangjin <wangjin@licaigc.com>
2016-10-06 11:03:32 +0800

    重构首页

--- demo/list.js
+++ demo/list.js
@@ -4,37 +4,43 @@ var data = {
             "image": "http://m.chanyouji.cn/index-cover/64695-2679221.jpg?imageView2/1/w/620/h/330/format/jpg",
             "avatar": "http://a.chanyouji.cn/34350/1468906824.jpg",
             "title": "梦想实现的地方-马达加斯加第二季",
-            "meta": "2013年8月10日 / 6天 / 476图"
+            "meta": "2013年8月10日 / 6天 / 476图",
+            "nickname": "SmallClock",
         },
         {
             "image": "http://m.chanyouji.cn/articles/625/ca9e50f74e273041e3a399bf5528f7b5.jpg?imageView2/1/w/620/h/330/format/jpg",
             "avatar": "http://tva3.sinaimg.cn/crop.0.0.540.540.50/6cbb1ee0jw8ej0zou5e71j20f00f0q3b.jpg",
             "title": "濑户内海北半圈-历史、艺术与兔子",
-            "meta": "2016年9月1日 / 6天 / 476图"
+            "meta": "2016年9月1日 / 6天 / 476图",
+            "nickname": "TinyClock",
         },
         {
             "image": "http://m.chanyouji.cn/articles/625/ca9e50f74e273041e3a399bf5528f7b5.jpg?imageView2/1/w/620/h/330/format/jpg",
             "avatar": "http://tva3.sinaimg.cn/crop.0.0.540.540.50/6cbb1ee0jw8ej0zou5e71j20f00f0q3b.jpg",
             "title": "濑户内海北半圈-历史、艺术与兔子",
-            "meta": "2016年9月1日 / 6天 / 476图"
+            "meta": "2016年9月1日 / 6天 / 476图",
+            "nickname": "狗小熊",
         },
         {
             "image": "http://m.chanyouji.cn/articles/625/ca9e50f74e273041e3a399bf5528f7b5.jpg?imageView2/1/w/620/h/330/format/jpg",
             "avatar": "http://tva3.sinaimg.cn/crop.0.0.540.540.50/6cbb1ee0jw8ej0zou5e71j20f00f0q3b.jpg",
             "title": "濑户内海北半圈-历史、艺术与兔子",
-            "meta": "2016年9月1日 / 6天 / 476图"
+            "meta": "2016年9月1日 / 6天 / 476图",
+            "nickname": "Bear",
         },
         {
             "image": "http://m.chanyouji.cn/articles/625/ca9e50f74e273041e3a399bf5528f7b5.jpg?imageView2/1/w/620/h/330/format/jpg",
             "avatar": "http://tva3.sinaimg.cn/crop.0.0.540.540.50/6cbb1ee0jw8ej0zou5e71j20f00f0q3b.jpg",
             "title": "濑户内海北半圈-历史、艺术与兔子",
-            "meta": "2016年9月1日 / 6天 / 476图"
+            "meta": "2016年9月1日 / 6天 / 476图",
+            "nickname": "山田さん",
         },
         {
             "image": "http://m.chanyouji.cn/articles/625/ca9e50f74e273041e3a399bf5528f7b5.jpg?imageView2/1/w/620/h/330/format/jpg",
             "avatar": "http://tva3.sinaimg.cn/crop.0.0.540.540.50/6cbb1ee0jw8ej0zou5e71j20f00f0q3b.jpg",
             "title": "濑户内海北半圈-历史、艺术与兔子",
-            "meta": "2016年9月1日 / 6天 / 476图"
+            "meta": "2016年9月1日 / 6天 / 476图",
+            "nickname": "小熊的日常",
         }
     ],
 
--- pages/item/item.js
+++ pages/item/item.js
@@ -8,11 +8,26 @@ var toolbar = [
 
 Page({
     data: {
+        // 当前日志详情
         diary: undefined,
+
+        // 右上角工具栏
         toolbar: toolbar,
+
+        // 图片预览模式
+        previewMode: false,
+
+        // 当前预览索引
+        previewIndex: 0,
+
+        // 多媒体内容列表
+        mediaList: [],
     },
 
-    onLoad: function(params) {
+    // 加载日记
+    getDiary(params) {
+        console.log("Loading diary data...")
+
         var id = params["id"], diary;
         if (id === undefined) {
             diary = diaries.diaries[0];
@@ -22,7 +37,37 @@ Page({
 
         this.setData({
             diary: diary,
-        })
+        });
+    },
+
+    // 过滤出预览图片列表
+    getMediaList() {
+        if (typeof this.data.diary !== 'undefined' &&
+            this.data.diary.list.length) {
+            this.setData({
+                mediaList: this.data.diary.list.filter(
+                    content => content.type === 'IMAGE'),
+            })
+        }
+    },
+
+    // 进入预览模式
+    enterPreviewMode(event) {
+        let url = event.target.dataset.src;
+        let urls = this.data.mediaList.map(media => media.content);
+        let previewIndex = urls.indexOf(url);
+
+        this.setData({previewMode: true, previewIndex});
+    },
+
+    // 退出预览模式
+    leavePreviewMode() {
+        this.setData({previewMode: false, previewIndex: 0});
+    },
+
+    onLoad: function(params) {
+        this.getDiary(params);
+        this.getMediaList();
     },
 
     onHide: function() {}
